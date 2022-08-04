fb0109d627ff54dc01df8f37d396b22cd4531de2
Code Cadet <codecadet@wozniak.local>
2022-08-04 17:15:22 +0000

    roomServices

--- src/main/java/org/canonymous/chat/services/RoomService.java
+++ src/main/java/org/canonymous/chat/services/RoomService.java
@@ -1,4 +1,19 @@
 package org.canonymous.chat.services;
 
+import org.canonymous.chat.command.dto.RoomDto;
+
+import java.util.List;
+
 public interface RoomService {
+
+    Room get(Integer id);
+
+    Room save(Room room);
+
+    void delete(Room room);
+
+    List<Room> listRooms();
+
+
+
 }
--- src/main/java/org/canonymous/chat/services/RoomServiceImpl.java
+++ src/main/java/org/canonymous/chat/services/RoomServiceImpl.java
@@ -1,4 +1,40 @@
 package org.canonymous.chat.services;
 
-public class RoomServiceImpl {
+import org.canonymous.chat.command.dto.RoomDto;
+
+import java.util.List;
+
+public class RoomServiceImpl implements RoomService{
+
+    RoomDao roomDao;
+
+
+
+    public void setRoomDao(RoomDao roomDao) {
+        this.roomDao = roomDao;
+    }
+
+    public RoomDao getRoomDao() {
+        return roomDao;
+    }
+
+    @Override
+    public Room get(Integer id) {
+        return null;
+    }
+
+    @Override
+    public Room save(Room room) {
+        return null;
+    }
+
+    @Override
+    public void delete(Room room) {
+
+    }
+
+    @Override
+    public List<Room> listRooms() {
+        return null;
+    }
 }
